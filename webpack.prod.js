5c03113f1c35ce354c792e985d1919caa9dabab3
abdul <abdul.rahmat@360kredi.id>
2024-07-30 17:03:58 +0700

    set output config

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,27 +1,32 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common");
+const TerserPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
 
 module.exports = merge(common, {
-  mode: 'production',
+  mode: "production",
   devtool: false,
   performance: {
     maxEntrypointSize: 90000,
-    maxAssetSize: 900000
+    maxAssetSize: 900000,
   },
   optimization: {
     minimizer: [
       new TerserPlugin({
         terserOptions: {
           output: {
-            comments: false
-          }
-        }
-      })
-    ]
+            comments: false,
+          },
+        },
+      }),
+    ],
   },
-  plugins: [
-    new CleanWebpackPlugin(),
-  ]
-})
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].[contenthash].js",
+    publicPath: "/",
+  },
+  plugins: [new CleanWebpackPlugin()],
+});
